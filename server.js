540c3a411e8be00923073a0ce6bcf93bd1049bb5
pbrudny <pbrudny@gmail.com>
2017-10-28 12:35:15 +0200

    Add moving dots

--- server.js
+++ server.js
@@ -7,8 +7,36 @@ var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
 
+var players = {};
 io.on('connection', function(socket) {
+    socket.on('new player', function() {
+        console.log('New guy: ', socket.id);
+        players[socket.id] = {
+            x: 300,
+            y: 300
+        }
+    });
+    socket.on('movement', function(data) {
+        var player = players[socket.id] || {};
+        if(data.left) {
+            player.x -= 5;
+        }
+        if(data.right) {
+            player.x += 5;
+        }
+        if(data.up) {
+            player.y -= 5;
+        }
+        if(data.down) {
+            player.y += 5;
+        }
+    });
 });
+
+setInterval(function() {
+    io.sockets.emit('state', players);
+}, 1000/60);
+
 
 app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
@@ -20,7 +48,3 @@ app.get('/', function(req, res) {
 server.listen(5000, function() {
     console.log('Starting server on port 5000');
 });
-
-setInterval(function() {
-    io.sockets.emit('message', 'hi!');
-}, 1000);
