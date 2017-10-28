342084ba2beca9900ac2ad876d8c6f38ef94436f
pbrudny <pbrudny@gmail.com>
2017-10-28 12:35:39 +0200

    Add moving dots

--- static/game.js
+++ static/game.js
@@ -5,13 +5,14 @@ canvas.width = 800;
 canvas.height = 600;
 
 var context = canvas.getContext('2d');
+
 socket.on('state', function(players) {
     context.clearRect(0, 0, 800, 600);
     context.fillStyle = 'green';
     for (var id in players) {
         var player = players[id];
         context.beginPath();
-        context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+        context.arc(player.x, player.y, 20, 0, 2 * Math.PI);
         context.fill();
     }
 });
